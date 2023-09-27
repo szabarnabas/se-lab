77e8e9488211951dd72f9dabd402359b27aa67c9
Szanto Barnabas <barniszanto11@gmail.com>
2023-09-27 17:28:54 +0200

    branch-b megvan

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,6 +80,7 @@ public class GT4500 implements SpaceShip {
         if(! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1);
           firingSuccess = secondaryTorpedoStore.fire(1); 
+        //branch-B
         }
         break;
     }
