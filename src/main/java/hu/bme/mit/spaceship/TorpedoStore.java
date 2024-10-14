99a32476cbeac32cd05092b38573517f9cdd4cab
szabarnabas <93016236+szabarnabas@users.noreply.github.com>
2024-10-14 16:05:21 +0200

    3. error solved

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -29,6 +29,7 @@ public class TorpedoStore {
   }
 
   public boolean fire(int numberOfTorpedos){
+    
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
     }
